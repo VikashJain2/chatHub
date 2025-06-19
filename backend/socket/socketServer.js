b6ae1d6bd9fbda9a624c9ea8f70d1ce478f16c7e
Vikashjain2 <vikkijain991@gmail.com>
2025-06-19 13:31:26 +0530

    Refactor socket server to manage online users and enhance user status checking

--- backend/socket/socketServer.js
+++ backend/socket/socketServer.js
@@ -1,8 +1,9 @@
 import { Server } from "socket.io";
-
+import db from "../config/db.js";
 let io;
 
 export function initSocketServer(httpServer) {
+  let connection;
   io = new Server(httpServer, {
     cors: {
       origin: process.env.FRONTEND_URL,
@@ -10,11 +11,14 @@ export function initSocketServer(httpServer) {
     },
   });
 
-  io.on('connection', (socket) => {
+  const onlineUsers = new Map();
+
+  io.on("connection", (socket) => {
     const userId = socket.handshake?.auth?.userId;
 
     if (userId) {
       socket.join(`user:${userId}`);
+      onlineUsers.set(userId, socket.id);
       console.log(`User ${userId} connected with socket ${socket.id}`);
     } else {
       console.warn(`Socket ${socket.id} connected without auth userId`);
@@ -26,7 +30,37 @@ export function initSocketServer(httpServer) {
       }
     });
 
-    socket.on('disconnect', () => {
+    socket.on("check-user-status", async ({ friendId }) => {
+      if (onlineUsers.has(friendId)) {
+        socket.emit("user-status-response", {
+          friendId,
+          isOnline: true,
+        });
+      } else {
+        connection = await db.getConnection();
+        const [row] = await connection.query(
+          "SELECT last_seen FROM user WHERE id = ?",
+          [friendId]
+        );
+        connection.release();
+        socket.emit("user-status-response", {
+          friendId,
+          isOnline: false,
+          lastSeen: row.last_seen || null,
+        });
+      }
+    });
+
+    socket.on("disconnect", async () => {
+      if (userId) {
+        onlineUsers.delete(userId);
+        connection = await db.getConnection();
+
+        await connection.query("UPDATE user SET last_seen = ? WHERE id = ?", [
+          new Date(),
+          userId,
+        ]);
+      }
       console.log(`Socket ${socket.id} disconnected`);
     });
   });
